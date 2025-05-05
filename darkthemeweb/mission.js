fdaeefabb39625144da5697d873d70cecc208b0b
BRAXTONBUTLER2 <braxtonbutler@byui.edu>
2025-05-05 17:57:17 -0600

    fixedphotostry3

--- darkthemeweb/mission.js
+++ darkthemeweb/mission.js
@@ -1,20 +1,23 @@
-
 const themeSelector = document.querySelector('#theme-selector');
 const body = document.body;
 const logo = document.querySelector('#logo');
-
 
 function changeTheme() {
   const currentTheme = themeSelector.value;
 
   if (currentTheme === 'dark') {
     body.classList.add('dark');
+    body.classList.remove('light');
     logo.setAttribute('src', 'images/BYUILOGOWHITE.png');
   } else {
+    body.classList.add('light');
     body.classList.remove('dark');
     logo.setAttribute('src', 'images/BYUILOGOBLACK.png');
   }
 }
 
+// Set initial theme on page load
+changeTheme();
 
+// Listen for changes
 themeSelector.addEventListener('change', changeTheme);
